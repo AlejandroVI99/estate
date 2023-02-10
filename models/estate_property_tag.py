f930f245ed5c3abba2d9e163ef3886cf77206cf1
Alejandro Vazquez Ibarra <alejandrovi@vauxoo.com>
2023-02-10 16:36:11 -0600

    [ADD] Estate: Odoo Getting Started Cap 1-8
    Se realizaron los capitulos 1 a 8 del Getting Started de Odoo, en los cuales se
    creaba un nuevo modulo llamado estate, el cual se enfoca en el area de negocio Real Estate.

---
+++ models/estate_property_tag.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+class EstatePropertyTag(models.Model):
+    _name = 'estate.property.tag'
+    _description = "Property Tag Model"
+
+    name = fields.Char(required=True)
+    
